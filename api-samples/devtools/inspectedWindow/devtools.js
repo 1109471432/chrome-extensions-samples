2fdc4c31ceb1c2ec51895e99688e1f78bb2bd1d5
11094 <1109471432@qq.com>
2025-04-24 13:42:30 +0800

    init

--- api-samples/devtools/inspectedWindow/devtools.js
+++ api-samples/devtools/inspectedWindow/devtools.js
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 chrome.devtools.panels.create('demo panel', 'icon.png', 'panel.html', () => {
   console.log('user switched to this panel');
 });
