4c1cdf2373fb80b86eba5022622410a6ab61f750
11094 <1109471432@qq.com>
2025-04-29 13:16:18 +0800

    add request

--- api-samples/devtools/inspectedWindow/background.js
+++ api-samples/devtools/inspectedWindow/background.js
@@ -1,7 +1,17 @@
-console.log('background loaded.');
 chrome.debugger.onEvent.addListener(function (source, method, params) {
   if (method === 'Network.responseReceived') {
     console.log('Response received:', params.response);
     // Perform your desired action with the response data
   }
 });
+console.log('background loaded.');
+// 发送一个 GET 请求
+fetch('https://zh.z-library.sk/', {
+  mode: 'no-cors'
+})
+  .then((response) => {
+    console.log('fetch Response received:', response);
+  })
+  .catch((error) => {
+    console.error('Fetch error:', error);
+  });
